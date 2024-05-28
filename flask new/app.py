080c1155fbf9fdd0ddb0ecee70cdc71bae4732e0
Paul <157764547+Safaero@users.noreply.github.com>
2024-05-27 20:13:24 -0600

    flask repppo

--- flask new/app.py
+++ flask new/app.py
@@ -1,5 +1,36 @@
-from flask import Flask
+from flask import Flask,request
 
 app= Flask(__name__)
+
+#ruta basica
+@app.route('/')
+def principal():
+    return 'Hola mundo Flask'
+
+#ruta basica
+@app.route('/Usuario')
+@app.route('/Saludar')
+def saludos():
+    return 'Hola Peredo ja'
+
+#rutas con parametros
+
+@app.route('/hi/nombre>')
+def hi (nombre):
+    return 'hola'+ nombre +'!!!'
+
+#definicion demetodos detrabajo
+
+@app.route('/formulario', methods=['GET','POST'])
+def formulario():
+    if request.method == 'GET':
+        return 'No es seguro enviar pass por GET'
+    elif request.method == 'POST':
+        return 'POST si esz seguro'
+
+@app.errorhandler(404)
+def paginando(e):
+    return 'revisa tu sintaxis: no encontrado naada'
+
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
