c58042429a7042fb2a8ed8e68bc20a20d8c499d4
Paul <157764547+Safaero@users.noreply.github.com>
2024-06-10 20:31:43 -0600

    practica 5

--- CrudAlbums/app.py
+++ CrudAlbums/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for, redirect, flash
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'bdflask'
+
+app.secret_key ='mysecretkey'
 
 mysql=MySQL(app)
 
@@ -15,11 +17,15 @@ def index():
 @app.route('/guardarAlbum',methods=['POST'])
 def guardarAlbum():
     if request.method == 'POST':
-        titulo= request.form ['txtTitulo']
-        artista= request.form ['txtArtista']
-        anio= request.form ['txtAnio']
-        print(titulo,artista,anio)
-        return 'datos recibidos en el servidor'
+        ftitulo= request.form ['txtTitulo']
+        fartista= request.form ['txtArtista']
+        fanio= request.form ['txtAnio']
+        #print(titulo,artista,anio)
+        cursor = mysql.connection.cursor()
+        cursor.execute('INSERT INTO albums (titulo,artista,anio) VALUES (%s,%s,%s)',(ftitulo,fartista,fanio))
+        mysql.connection.commit()
+        flash ('album guardado correctamente')
+        return redirect(url_for('index'))
      
 @app.errorhandler(404)     
 def paginando(e):
