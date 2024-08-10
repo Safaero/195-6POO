844bfae6b3cf1255a11f44566ec338b3cf751f9d
cristopher-loyola <157190820+cristopher-loyola@users.noreply.github.com>
2024-08-09 22:40:00 -0600

    tiene vista de admin y usuario
    , acomode las navbar para los admin y usuarios a mi logica, revisenlo y me dicen si esta bien

--- pruebas flask/app.py
+++ pruebas flask/app.py
@@ -10,6 +10,17 @@ app.secret_key = 'mysecretkey'
 
 mysql = MySQL(app)
 
+from flask import Flask, request, render_template, url_for, redirect, flash, session
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'bdflask'
+app.secret_key = 'mysecretkey'
+
+mysql = MySQL(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
@@ -17,22 +28,40 @@ def login():
         rfc = request.form['rfc']
         contraseña = request.form['contraseña']
         cursor = mysql.connection.cursor()
-        cursor.execute('SELECT nombre FROM tb_medicos WHERE rfc = %s AND contraseña = %s', (rfc, contraseña))
+        
+
+        cursor.execute('SELECT nombre, id_roles FROM tb_medicos WHERE rfc = %s AND contraseña = %s', (rfc, contraseña))
         medico = cursor.fetchone()
         
         if medico:
-            session['nombre_medico'] = medico[0] 
-            return redirect(url_for('home'))
+            session['nombre_medico'] = medico[0]
+            session['id_roles'] = medico[1]
+            
+           
+            if medico[1] == 1:
+                return redirect(url_for('admin_home'))
+            else:  
+                return redirect(url_for('home'))
         else:
             flash('Credenciales incorrectas. Inténtalo de nuevo.')
             return redirect(url_for('login'))
+    
     return render_template('login.html')
-
 
 @app.route('/home')
 def home():
+    if 'nombre_medico' not in session:
+        return redirect(url_for('login'))
+    
     nombre_medico = session.get('nombre_medico', 'Doctor')
-    return render_template('index.html', nombre_medico=nombre_medico)
+    return render_template('index.html', nombre_medico=nombre_medico)  
+
+@app.route('/admin_home')
+def admin_home():
+    if 'nombre_medico' not in session:
+        return redirect(url_for('login'))
+    
+    return render_template('admin.html')  
 
 @app.route('/registros')
 def registros():
